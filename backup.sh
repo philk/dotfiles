97fac7f9300b36dd2c38041a28a41977ce67cc4c
Phil Kates <me@philkates.com>
2010-07-26 15:08:20 -0700

    Removed old style backup.sh

--- backup.sh
+++
@@ -1,8 +0,0 @@
-cp ~/.vimrc ./vimrc
-cp ~/.gvimrc ./gvimrc
-cp ~/.irbrc ./irbrc
-cp ~/.screenrc ./screenrc
-cp ~/.bash_profile ./bash_profile
-cp ~/.bashrc ./bashrc
-rm -rf ./vim
-cp -Rp ~/.vim ./vim
